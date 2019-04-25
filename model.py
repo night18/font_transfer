539c393f7d252e652befff17a33c0eca78c36a61
night18 <warrior.sp@gmail.com>
2019-04-25 16:30:16 -0400

    https://github.com/eriklindernoren/Keras-GAN/blob/master/cyclegan/cyclegan.py

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.python.keras.layers import Lambda
 
-gen_filter_num = 64
+gen_filter_num = 32
 dis_filter_num = 64
 batch_size = 1
 pool_size = 50
@@ -26,129 +26,130 @@ img_height = 256
 img_depth = 3
 relu_alpha = 0.05
 dropout_rate = 0.2
+learning_rate = 0.01
 
-def shortcutModule(pre_lyr, output_channels, res_id, is_stride = False):
-	if is_stride:
-		shortcut = MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same', name=res_id+'_shortcut_pool')(pre_lyr)
-	else:
-		shortcut = pre_lyr
-
-	# shortcut = Conv2D(output_channels, kernel_size=(1,1), padding='same', name=res_id+'shortcut_conv')(shortcut)
-	return shortcut
-
-def myResModule(pre_lyr, kernel_length, first_channels, second_channels, res_id, is_stride =False):
-	if is_stride:
-		x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(2,2) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
-	else:
-		x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
-	x = BatchNormalization(name=res_id+'_batch_1')(x)
-	x = Dropout(dropout_rate, name=res_id+'_dropout_1')(x)
-	x = LeakyReLU(alpha=0.relu_alpha)(x)
-
-	x = Conv2D(second_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1), padding='same', name=res_id+'_conv_2')(x)
-	
-	shortcut = shortcutModule(pre_lyr, second_channels, res_id, is_stride)
-	x = concatenate([x, shortcut], name=res_id+'_conc')
-	x = BatchNormalization(name=res_id+'_batch_2')(x)
-	x = Dropout(dropout_rate, name=res_id+'_dropout_2')(x)
-	x = LeakyReLU(alpha=0.relu_alpha)(x)
-	return x
-
-def encoder(pre_lyr, name=''):
-	x = Conv2D(gen_filter_num, kernel_size=(7,7), strides=(1,1), padding='same', name= name+'_encoder_conv_1')(pre_lyr)
-	x = BatchNormalization(name= name+'_encoder_norm_1')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_encoder_relu_1')(x)
-
-	x = Conv2D( 2 * gen_filter_num , kernel_size=(3,3), strides=(2,2), padding='same', name= name+'_encoder_conv_2')(x)
-	x = BatchNormalization(name= name+'_encoder_norm_2')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_encoder_relu_2')(x)
-
-	x = Conv2D( 4 * gen_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name= name+'_encoder_conv_3')(x)
-	x = BatchNormalization(name= name+'_encoder_norm_3')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_encoder_relu_3')(x)
-	
-	return x
+class CycleGAN():
+	def __init__(self):
+		# Todo: Load data
+		optimizers = Adam(learning_rate)
+		
+		#Build and compile the discriminator
+		self.d_A = self.discriminator(name='d_A')
+		self.d_B = 
+		# Chun
 
 
-def transfer(pre_lyr, name=''):
-	x = myResModule(pre_lyr, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_1')
-	x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_2')
-	x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_3')
-	x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_4')
-	x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_5')
-	x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_6')
+	def shortcutModule(pre_lyr, output_channels, res_id, is_stride = False):
+		if is_stride:
+			shortcut = MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same', name=res_id+'_shortcut_pool')(pre_lyr)
+		else:
+			shortcut = pre_lyr
 
-	return x
+		# shortcut = Conv2D(output_channels, kernel_size=(1,1), padding='same', name=res_id+'shortcut_conv')(shortcut)
+		return shortcut
 
-def decoder(pre_lyr, name=''):
-	x = Conv2DTranspose( 2 * gen_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name=name+'_decoder_conv_1')(pre_lyr)
-	x = BatchNormalization(name= name+'_decoder_norm_1')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_decoder_relu_1')(x)
+	def myResModule(pre_lyr, kernel_length, first_channels, second_channels, res_id, is_stride =False):
+		if is_stride:
+			x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(2,2) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
+		else:
+			x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
+		x = BatchNormalization(name=res_id+'_batch_1')(x)
+		x = Dropout(dropout_rate, name=res_id+'_dropout_1')(x)
+		x = LeakyReLU(alpha=0.relu_alpha)(x)
 
-	x = Conv2DTranspose( gen_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name=name+'_decoder_conv_2')(x)
-	x = BatchNormalization(name= name+'_decoder_norm_2')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_decoder_relu_2')(x)
-
-	x = Conv2DTranspose( img_depth, kernel_size=(7,7), strides=(1,1), padding='same', name=name+'_decoder_conv_3')(x)
-	x = BatchNormalization(name= name+'_decoder_norm_3')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_decoder_relu_3')(x) #TODO: Not sure
-
-
-def generator(input_gen , name=''):
-	x = encoder(input_gen, name=name)
-	x = transfer(x, name=name)
-	output_gen = decoder(x, name=name)
-	return output_gen
-
-def discriminator(input_dis, name=''):
-	x = Conv2D(dis_filter_num, kernel_size=(5,5), strides=(2,2), padding='same', name= name+'_disc_conv_1')(pre_lyr)
-	x = BatchNormalization(name= name+'_disc_norm_1')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_disc_relu_1')(x)
-
-	x = Conv2D( 2 * dis_filter_num, kernel_size=(5,5), strides=(2,2), padding='same', name= name+'_disc_conv_2')(x)
-	x = BatchNormalization(name= name+'_disc_norm_2')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_disc_relu_2')(x)
-
-	x = Conv2D( 4 * dis_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name= name+'_disc_conv_3')(x)
-	x = BatchNormalization(name= name+'_disc_norm_3')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_disc_relu_3')(x)
-
-	x = Conv2D( 4 * dis_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name= name+'_disc_conv_4')(x)
-	x = BatchNormalization(name= name+'_disc_norm_4')(x)
-	x = LeakyReLU(alpha=relu_alpha, name= name+'_disc_relu_4')(x)
-
-	prediction = Dense( 1, activation='sigmoid' , padding='same', name= name+'_disc_conv_pred')(x)
-	
-	return prediction
-
-def cycleGAN():
-	input_Chinese = inputs(shape=(img_height, img_width, img_depth), name='input_Chinese')
-	# input_English = inputs(shape=(img_height, img_width, img_depth), name='input_English')
-
-	gen_English = generator(input_Chinese, name='g_c2e')
-	gen_Chinese = generator(input_English, name='g_e2c')
-
-	dis_Chinese = discriminator(input_Chinese, name='d_true_c')
-	dis_English = discriminator(input_English, name='d_true_e')
-
-	dis_gen_Chinese = discriminator(gen_Chinese, name='d_gen_c')
-	dis_gen_English = discriminator(gen_English, name='d_gen_c')
-
-	cyc_Chinese = generator(gen_English, name='cyc_c2e2c')
-	cyc_English = generator(gen_Chinese, name='cyc_e2c2e')
+		x = Conv2D(second_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1), padding='same', name=res_id+'_conv_2')(x)
+		
+		shortcut = shortcutModule(pre_lyr, second_channels, res_id, is_stride)
+		x = concatenate([x, shortcut], name=res_id+'_conc')
+		x = BatchNormalization(name=res_id+'_batch_2')(x)
+		x = Dropout(dropout_rate, name=res_id+'_dropout_2')(x)
+		x = LeakyReLU(alpha=0.relu_alpha)(x)
+		return x
 
 
-# discriminator loss
-# if the input is ground truth, the prediction should be close to 1(True)(yTrue = 1)
-# if the input is generative, the prediction should be close to 0(False)(yTrue = 0)
-def net_d_loss(yTrue, yPred):
-	return K.mean( K.square(yTrue - yPred) )
+	def generator(self, name = ''):
+		def encoder(pre_lyr, name=''):
+			x = Conv2D(gen_filter_num, kernel_size=(7,7), strides=(1,1), padding='same', name= name+'_encoder_conv_1')(pre_lyr)
+			x = BatchNormalization(name= name+'_encoder_norm_1')(x)
+			x = LeakyReLU(alpha=relu_alpha, name= name+'_encoder_relu_1')(x)
 
-# generator should create the image which is similar to the groud truth, i.e., dis_gen_Chinese should be 1
-def net_g_loss(yTrue, yPred):
-	return K.mean( K.square(1 - yPred) )
+			x = Conv2D( 2 * gen_filter_num , kernel_size=(3,3), strides=(2,2), padding='same', name= name+'_encoder_conv_2')(x)
+			x = BatchNormalization(name= name+'_encoder_norm_2')(x)
+			x = LeakyReLU(alpha=relu_alpha, name= name+'_encoder_relu_2')(x)
 
-def cycle_loss(yTrue, yPred):
-	return K.mean (K.abs(yTrue, yPred) )
+			x = Conv2D( 4 * gen_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name= name+'_encoder_conv_3')(x)
+			x = BatchNormalization(name= name+'_encoder_norm_3')(x)
+			x = LeakyReLU(alpha=relu_alpha, name= name+'_encoder_relu_3')(x)
+			
+			return x
 
-	
+
+		def transfer(pre_lyr, name=''):
+			x = myResModule(pre_lyr, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_1')
+			x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_2')
+			x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_3')
+			x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_4')
+			x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_5')
+			x = myResModule(x, 3, 4 * gen_filter_num, 4 * gen_filter_num, name+'_res_6')
+
+			return x
+
+		def decoder(pre_lyr, name=''):
+			x = Conv2DTranspose( 2 * gen_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name=name+'_decoder_conv_1')(pre_lyr)
+			x = BatchNormalization(name= name+'_decoder_norm_1')(x)
+			x = LeakyReLU(alpha=relu_alpha, name= name+'_decoder_relu_1')(x)
+
+			x = Conv2DTranspose( gen_filter_num, kernel_size=(3,3), strides=(2,2), padding='same', name=name+'_decoder_conv_2')(x)
+			x = BatchNormalization(name= name+'_decoder_norm_2')(x)
+			x = LeakyReLU(alpha=relu_alpha, name= name+'_decoder_relu_2')(x)
+
+			x = Conv2DTranspose( img_depth, kernel_size=(7,7), activation='tanh' ,strides=(1,1), padding='same', name=name+'_decoder_conv_3')(x)
+
+
+		img = Input(shape=(img_height, img_width, img_depth))
+
+		x1 = encoder(img, name=name)
+		x2 = transfer(x1, name=name)
+		output_gen = decoder(x2, name=name)
+
+		model = Model(
+			inputs = img,
+			outputs = output_gen)
+
+		return model
+
+	def discriminator(self, name = ''):
+
+		def d_layer(pre_lyr, filters, kernel_size=(3,3), name=''):
+			x = Conv2D(filters, kernel_size=kernel_size, strides=(2,2), padding='same', name= 'disc_conv_'+ name)(pre_lyr)
+			x = BatchNormalization(name= 'disc_norm_'+ name)(x)
+			x = LeakyReLU(alpha=relu_alpha, name= 'disc_relu_'+ name)(x)
+
+		img = Input(shape=(img_height, img_width, img_depth))
+
+		x1 = d_layer(img, dis_filter_num, kernel_size=(5,5), name='1')
+		x2 = d_layer(x1, 2 * dis_filter_num, kernel_size=(5,5), name='2')
+		x3 = d_layer(x2, 4 * dis_filter_num, kernel_size=(3,3), name='3')
+		x4 = d_layer(x3, 4 * dis_filter_num, kernel_size=(3,3), name='3')
+
+		prediction = Dense( 1, activation='sigmoid' , padding='same', name= name+'_disc_conv_pred')(x4)
+		model = Model(
+			inputs = img,
+			outputs = prediction
+		)
+		return model
+
+
+
+	# discriminator loss
+	# if the input is ground truth, the prediction should be close to 1(True)(yTrue = 1)
+	# if the input is generative, the prediction should be close to 0(False)(yTrue = 0)
+	def net_d_loss(yTrue, yPred):
+		return K.mean( K.square(yTrue - yPred) )
+
+	# generator should create the image which is similar to the groud truth, i.e., dis_gen_Chinese should be 1
+	def net_g_loss(yTrue, yPred):
+		return K.mean( K.square(1 - yPred) )
+
+	def cycle_loss(yTrue, yPred):
+		return K.mean (K.abs(yTrue, yPred) )
---
+++ test.py
@@ -0,0 +1,44 @@
+import os
+from fontTools.ttLib import TTFont
+from PIL import Image, ImageDraw, ImageFont
+
+TEXTS_DIR = "texts"
+IMAGES_DIR = "images"
+TTF_NAME = "Roboto-Regular"
+TTF_PATH = "Roboto-Regular.ttf"
+FONT_SIZE = "400"
+
+
+ttf = TTFont(TTF_PATH)
+print (ttf)
+for x in ttf["cmap"].tables:
+    for y in x.cmap.items():
+        char_unicode = chr(y[0])
+        char_utf8 = char_unicode.encode('utf_8')
+        # print(char_utf8)
+
+        char_name = y[1]
+        f = open(os.path.join(TEXTS_DIR, char_name + '.txt'), 'wb')
+        f.write(char_utf8)
+        f.close()
+ttf.close()
+
+font = ImageFont.truetype(TTF_PATH, 500)
+files = os.listdir(TEXTS_DIR)
+for filename in files:
+    name, ext = os.path.splitext(filename)
+    # print(name)
+    input_txt = TEXTS_DIR + "/" + filename
+    output_png = IMAGES_DIR + "/" + TTF_NAME + "/" + name + "_" + FONT_SIZE + ".png"
+
+    img = Image.new('RGB', (534, 560), color='white')
+    d = ImageDraw.Draw(img)
+
+    with open(os.path.join(TEXTS_DIR, filename)) as f:
+        c = f.read(1)
+        d.text((80,0), c, fill=(0,0,0), font=font)
+        img.save(output_png)
+
+# #     subprocess.call(["convert", "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba(0,0,0,0)", "label:@" + input_txt, output_png])
+
+# print("finished")
