1f21071f75ed52972d0967877fbc3c1f04eea203
night18 <warrior.sp@gmail.com>
2019-04-26 12:06:32 -0400

    bring home again

--- model.py
+++ model.py
@@ -16,10 +16,14 @@ import tensorflow.keras.backend as K
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.python.keras.layers import Lambda
+from data_loader import DataLoader
+import datetime
+import matplotlib.pyplot as plt
 
 gen_filter_num = 32
 dis_filter_num = 64
 batch_size = 1
+sample_interval = 50
 pool_size = 50
 img_width = 256
 img_height = 256
@@ -31,20 +35,28 @@ lambda_cycle = 10
 
 class CycleGAN():
 	def __init__(self):
-		# Todo: Load data
-		optimizers = Adam(learning_rate)
+
+
+		self.dataset_name = 'font2font'
+		self.data_loader = DataLoader(dataset_name=self.dataset_name, img_res = (img_height, img_width))
+
+		# Calculate output shape of D (PatchGAN)
+		patch = int(img_height / 2**4)
+		self.disc_patch = (patch, patch, 1)
+
+		optimizer = Adam(learning_rate)
 		
 		# Build and compile the discriminator
 		self.d_A = self.discriminator(name='d_A')
 		self.d_B = self.discriminator(name='d_B')
 		self.d_A.compile(
 			loss = 'mse',
-			optimizers = optimizers,
+			optimizer = optimizer,
 			metrics = ['accuracy']
 			)
 		self.d_B.compile(
 			loss = 'mse',
-			optimizers = optimizers,
+			optimizer = optimizer,
 			metrics = ['accuracy']
 			)
 
@@ -80,42 +92,37 @@ class CycleGAN():
 		self.combine.compile(
 						loss = ['mse', 'mse', 'mae', 'mae'],
 						loss_weights = [1, 1, lambda_cycle, lambda_cycle],
-						optimizers = optimizers
+						optimizer = optimizer
 						)
 		
-
-		# Chun
-
-
-	def shortcutModule(pre_lyr, output_channels, res_id, is_stride = False):
-		if is_stride:
-			shortcut = MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same', name=res_id+'_shortcut_pool')(pre_lyr)
-		else:
-			shortcut = pre_lyr
-
-		# shortcut = Conv2D(output_channels, kernel_size=(1,1), padding='same', name=res_id+'shortcut_conv')(shortcut)
-		return shortcut
-
-	def myResModule(pre_lyr, kernel_length, first_channels, second_channels, res_id, is_stride =False):
-		if is_stride:
-			x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(2,2) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
-		else:
-			x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
-		x = BatchNormalization(name=res_id+'_batch_1')(x)
-		x = Dropout(dropout_rate, name=res_id+'_dropout_1')(x)
-		x = LeakyReLU(alpha=0.relu_alpha)(x)
-
-		x = Conv2D(second_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1), padding='same', name=res_id+'_conv_2')(x)
-		
-		shortcut = shortcutModule(pre_lyr, second_channels, res_id, is_stride)
-		x = concatenate([x, shortcut], name=res_id+'_conc')
-		x = BatchNormalization(name=res_id+'_batch_2')(x)
-		x = Dropout(dropout_rate, name=res_id+'_dropout_2')(x)
-		x = LeakyReLU(alpha=0.relu_alpha)(x)
-		return x
-
-
 	def generator(self, name = ''):
+		def shortcutModule(pre_lyr, output_channels, res_id, is_stride = False):
+			if is_stride:
+				shortcut = MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same', name=res_id+'_shortcut_pool')(pre_lyr)
+			else:
+				shortcut = pre_lyr
+
+			# shortcut = Conv2D(output_channels, kernel_size=(1,1), padding='same', name=res_id+'shortcut_conv')(shortcut)
+			return shortcut
+
+		def myResModule(pre_lyr, kernel_length, first_channels, second_channels, res_id, is_stride =False):
+			if is_stride:
+				x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(2,2) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
+			else:
+				x = Conv2D(first_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1) ,padding='same', name=res_id+'_conv_1' )(pre_lyr)
+			x = BatchNormalization(name=res_id+'_batch_1')(x)
+			x = Dropout(dropout_rate, name=res_id+'_dropout_1')(x)
+			x = LeakyReLU(alpha=relu_alpha)(x)
+
+			x = Conv2D(second_channels, kernel_size=(kernel_length,kernel_length), strides=(1,1), padding='same', name=res_id+'_conv_2')(x)
+			
+			shortcut = shortcutModule(pre_lyr, second_channels, res_id, is_stride)
+			x = concatenate([x, shortcut], name=res_id+'_conc')
+			x = BatchNormalization(name=res_id+'_batch_2')(x)
+			x = Dropout(dropout_rate, name=res_id+'_dropout_2')(x)
+			x = LeakyReLU(alpha=relu_alpha)(x)
+			return x
+
 		def encoder(pre_lyr, name=''):
 			x = Conv2D(gen_filter_num, kernel_size=(7,7), strides=(1,1), padding='same', name= name+'_encoder_conv_1')(pre_lyr)
 			x = BatchNormalization(name= name+'_encoder_norm_1')(x)
@@ -152,7 +159,7 @@ class CycleGAN():
 			x = LeakyReLU(alpha=relu_alpha, name= name+'_decoder_relu_2')(x)
 
 			x = Conv2DTranspose( img_depth, kernel_size=(7,7), activation='tanh' ,strides=(1,1), padding='same', name=name+'_decoder_conv_3')(x)
-
+			return x
 
 		img = Input(shape=(img_height, img_width, img_depth))
 
@@ -172,15 +179,16 @@ class CycleGAN():
 			x = Conv2D(filters, kernel_size=kernel_size, strides=(2,2), padding='same', name= 'disc_conv_'+ name)(pre_lyr)
 			x = BatchNormalization(name= 'disc_norm_'+ name)(x)
 			x = LeakyReLU(alpha=relu_alpha, name= 'disc_relu_'+ name)(x)
+			return x
 
 		img = Input(shape=(img_height, img_width, img_depth))
 
 		x1 = d_layer(img, dis_filter_num, kernel_size=(5,5), name='1')
 		x2 = d_layer(x1, 2 * dis_filter_num, kernel_size=(5,5), name='2')
 		x3 = d_layer(x2, 4 * dis_filter_num, kernel_size=(3,3), name='3')
-		x4 = d_layer(x3, 4 * dis_filter_num, kernel_size=(3,3), name='3')
+		x4 = d_layer(x3, 4 * dis_filter_num, kernel_size=(3,3), name='4')
 
-		prediction = Dense( 1, activation='sigmoid' , padding='same', name= name+'_disc_conv_pred')(x4)
+		prediction = Dense( 1, activation='sigmoid', name= name+'_disc_conv_pred')(x4)
 		model = Model(
 			inputs = img,
 			outputs = prediction
@@ -188,16 +196,86 @@ class CycleGAN():
 		return model
 
 
+	def train(self, epochs, batch_size = 1, sample_interval = 200):
+		# TODO make the parameter self
 
-	# discriminator loss
-	# if the input is ground truth, the prediction should be close to 1(True)(yTrue = 1)
-	# if the input is generative, the prediction should be close to 0(False)(yTrue = 0)
-	def net_d_loss(yTrue, yPred):
-		return K.mean( K.square(yTrue - yPred) )
+		start_time = datetime.datetime.now()
 
-	# generator should create the image which is similar to the groud truth, i.e., dis_gen_Chinese should be 1
-	def net_g_loss(yTrue, yPred):
-		return K.mean( K.square(1 - yPred) )
+		# Adversarial loss ground truths
+		valid = np.ones((batch_size, ) + self.disc_patch )
+		fake = np.zeros((batch_size, ) + self.disc_patch )
 
-	def cycle_loss(yTrue, yPred):
-		return K.mean (K.abs(yTrue, yPred) )
+		for epoch in range(epochs):
+			for batch_i, (imgs_A, imgs_B) in enumerate(self.data_loader.load_batch(batch_size)):
+
+				# Train Discriminator
+
+				fake_B = self.g_AtoB.predict(imgs_A)
+				fake_A = self.g_BtoA.predict(imgs_B)
+
+				dA_loss_real = self.d_A.train_on_batch(imgs_A, valid)
+				dA_loss_fake = self.d_A.train_on_batch(fake_A, fake)
+				dA_loss = 0.5 * np.add(dA_loss_real, dA_loss_fake)
+
+				dB_loss_real = self.d_B.train_on_batch(imgs_B, valid)
+				dB_loss_fake = self.d_B.train_on_batch(fake_B, fake)
+				dB_loss = 0.5 * np.add(dB_loss_real, dB_loss_fake)
+
+				d_loss = 0.5 * np.add(dA_loss, dB_loss)
+
+				g_loss = self.combine.train_on_batch([imgs_A, imgs_B],
+													[valid, valid,
+													imgs_A, imgs_B])
+
+				elapsed_time = datetime.datetime.now() - start_time
+
+				# plot the progress
+				print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] [G loss: %05f, adv: %05f, recon: %05f] time: %s " \
+														% ( epoch, epochs,
+															batch_i, self.data_loader.n_batches,
+															d_loss[0], 100*d_loss[1],
+															g_loss[0],
+															np.mean(g_loss[1:3]),
+															np.mean(g_loss[3:5]),
+															elapsed_time))
+
+				# If at save interval => save generated image samples
+				if batch_i % sample_interval == 0:
+					self.sample_images(epoch, batch_i)
+
+
+	def sample_images(self, epoch, batch_i):
+		os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
+		r, c = 2, 3
+
+		imgs_A = self.data_loader.load_data(domain='A', batch_size=1, is_testing=True)
+		imgs_B = self.data_loader.load_data(domain='B', batch_size=1, is_testing=True)
+
+		fake_B = self.g_AtoB.predict(imgs_A)
+		fake_A = self.g_BtoA.predict(imgs_B)
+
+		recon_A = self.g_BtoA.predict(fake_B)
+		recon_B = self.g_AtoB.predict(fake_A)
+
+		gen_imgs = np.concatenate([imgs_A, fake_B, recon_A, imgs_B, fake_A, recon_B])
+
+		print(gen_imgs)
+		# rescale the images to 0 -1
+		gen_imgs = 0.5*gen_imgs + 0.5
+
+		titles = ['Original', 'Translated', 'Reconstructed']
+		fig, axs = plt.subplots(r, c)
+		cnt = 0
+
+		for i in range(r):
+			for j in range(c):
+				axs[i,j].imshow(gen_imgs[cnt])
+				axs[i,j].set_title(titles[j])
+				axs[i,j].axis('off')
+				cnt += 1
+		fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
+		plt.close()
+
+if __name__ == '__main__':
+	gan = CycleGAN()
+	gan.train(epochs=50, batch_size = 1,  sample_interval=sample_interval)
